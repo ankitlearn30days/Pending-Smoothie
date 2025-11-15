e1f19cca87edf5d4c5273673bbf64c085bb763b9
Ankit <ankit41298@gmail.com>
2025-11-16 01:58:35 +0530

    Update streamlit_app_pendingOrder.py
    Changes added for app to work

--- streamlit_app_pendingOrder.py
+++ streamlit_app_pendingOrder.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
-from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Pending Smoothie Order :cup_with_straw: ")
